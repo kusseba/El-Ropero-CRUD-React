f119b84a5a687ac8d9bf3a048f4a63e5644c2e05
Alex Cernik <57844660+AlexCernik@users.noreply.github.com>
2024-09-24 23:51:53 -0300

    Update Index.jsx

--- src/screens/index/Index.jsx
+++ src/screens/index/Index.jsx
@@ -143,30 +143,6 @@ const Index = () => {
 
         <Box mt={5} height={50} />
       </Container>
-
-      <Box component='footer' py={3}>
-        <Container maxWidth='lg' sx={{ px: { xs: 1, sm: 2, md: 3 } }}>
-          <Typography
-            color='#e6e6e6'
-            fontSize={20}
-            lineHeight={1}
-            fontFamily='Timing'
-          >
-            El Ropero
-          </Typography>
-          <Grid container spacing={{ xs: .5, sm: 1 }}>
-            <Grid component='ul' xs={6} sm={6} md={3} style={{ listStyleType: 'none', paddingInlineStart: 8 }}>
-              <Typography mb={.5} component='li' color='#e6e6e6' fontSize={14}>Contacto</Typography>
-              <Typography mb={.5} component='li' color='#e6e6e6' fontSize={14}>Ayuda</Typography>
-              <Typography component='li' color='#e6e6e6' fontSize={14}>Blog</Typography>
-            </Grid>
-
-            <Grid component='ul' xs={6} sm={6} md={3} style={{ listStyleType: 'none', paddingInlineStart: 8 }}>
-              <Typography component='li' color='#e6e6e6' fontSize={14}>Envíos</Typography>
-            </Grid>
-          </Grid>
-        </Container>
-      </Box>
     </>
   );
 };
